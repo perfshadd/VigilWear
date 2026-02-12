2acc0afd2f4e4f77280963dec5ee301d1b85ef93
SHAD Alotaibi <shahadmek1@gmail.com>
2026-02-12 03:56:11 +0300

    Fix CSS import path for Vercel

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/Login.css";
+import "../Styles/Login.css";
 function Login({ setIsLoggedIn }) {
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
